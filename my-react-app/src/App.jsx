feaa80d63461ec497a302564d58e3f4be4c7c695
padalanithya <padalanithya>
2025-05-13 15:34:50 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Weather from "./weather"  // Make sure the file exports component as 'Weather'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Weather />
+      </div>
+    </>
+  )
+}
+
+export default App
